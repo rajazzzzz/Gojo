e3ae57e537356ba2008b0e69f4bcc01ccf7f6e9d
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-08-03 16:16:50 +0530

    Update stream.py

--- plugins/Extra/stream/stream.py
+++ plugins/Extra/stream/stream.py
@@ -2,8 +2,8 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
 from info import STREAM_MODE, URL, LOG_CHANNEL
 from urllib.parse import quote_plus
-from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
-from TechVJ.util.human_readable import humanbytes
+from v3bot.util.file_properties import get_name, get_hash, get_media_file_size
+from v3bot.util.human_readable import humanbytes
 import humanize
 import random
 
