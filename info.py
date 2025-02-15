437006272458b0a800914748c5d5d8e9cee5c55b
Undefine ✨ <166386367+anujmore90@users.noreply.github.com>
2025-02-15 08:04:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinema_house_24x7')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/cinema_house_update')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/CMHmovies24x7')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/CMHmoviesOfficial')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'cinema_house_support') # Support Chat Link Without https:// or @
 
